fe1549f6b083157b4bd19b7a6c60c868f3aea0a8
Kiratpat <139159166+Kiratpat@users.noreply.github.com>
2026-02-18 17:23:37 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+//{number}
+
+let score; // string
+// let score = 44  //number
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(valueInNumber)
+
+// "33" = 33
+// "33abs" = NaN
+// true = 1; false = 0  
+
+//{boolean}
+
+let isLoggedIn = "_"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1 = true; 0 = false;
+// "" = false;
+// "_" = true;
+
+//{string}
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
