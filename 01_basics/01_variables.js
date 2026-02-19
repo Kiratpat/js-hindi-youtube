76db4b5e29b93cfec699183e549c23f0ecffefa2
Kiratpat <139159166+Kiratpat@users.noreply.github.com>
2026-02-19 13:26:09 +0000

    conversionOperator

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "kirat@google.com"
+var accountPassword = "123"
+accountCity = "Jaipur"
+
+
+accountEmail = "patel@google.com"
+accountPassword = "321"
+accountCity = "Delhi"
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
