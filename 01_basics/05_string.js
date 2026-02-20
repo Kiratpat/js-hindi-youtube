274af26b1e27a139809c15200b4a325dbc3a7c57
Kiratpat <139159166+Kiratpat@users.noreply.github.com>
2026-02-20 13:26:28 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,22 @@
+const name = "kirat"
+const age = 22
+
+// Concatenation
+//console.log(`My name is ${name} and I am ${age} years old.`)
+
+const fileName = new String('kirat patel')
+
+console.log(fileName.length);
+console.log(fileName.indexOf('p'));
+console.log(fileName.charAt(2));
+console.log(fileName.toUpperCase());
+console.log(fileName.toLowerCase());
+console.log(fileName.slice(0,-6));
+console.log(fileName.slice(1,6));
+console.log(fileName.split('a'));
+console.log(fileName.replace('patel', 'singh'));
+console.log(fileName.includes('patel'));
+
+const newStringOne = "   kiratpatel  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
